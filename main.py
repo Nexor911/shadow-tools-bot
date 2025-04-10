cec164351d154df5ba9208a40ecfe3d9cc351787
honor <honor@HONOR>
2025-04-10 15:50:35 +0300

    Добавлена команда для поиска по ip

--- main.py
+++ main.py
@@ -1,13 +1,69 @@
-import telebot;
-bot = telebot.TeleBot('Bot-token')
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "/tools":
-        bot.send_message(message.from_user.id, "https://osintframework.com/, https://inteltechniques.com/, https://www.shodan.io/, https://leakcheck.io/, https://www.tineye.com/")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "доступна только команда /tools")
-    else:
-        bot.send_message(message.from_user.id, "напиши /help")
-        
+import telebot
+import requests
+import logging
+
+# Токен бота
+bot = telebot.TeleBot('Token')
+
+# Настройка логов
+logging.basicConfig(
+    level=logging.INFO,
+    filename='logi.log',
+    filemode='a',
+    format='[%(asctime)s] %(levelname)s - %(message)s'
+)
+
+# Логируем старт бота
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    logging.info(f'Пользователь {message.from_user.id} написал /start')
+    bot.send_message(message.chat.id, "Hello, say /help")
+
+# Логируем команду /help
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    logging.info(f'Пользователь {message.from_user.id} написал /help')
+    bot.send_message(message.chat.id, "Доступные команды:\n/tools\n/ip [ip адрес]")
+
+# Логируем команду /tools
+@bot.message_handler(commands=['tools'])
+def tools_command(message):
+    logging.info(f'Пользователь {message.from_user.id} написал /tools')
+    bot.send_message(message.chat.id, "https://osintframework.com/, https://inteltechniques.com/, https://www.shodan.io/, https://leakcheck.io/, https://www.tineye.com/")
+
+# Функция поиска по IP
+def ip_lookup(ip):
+    url = f"http://ip-api.com/json/{ip}"
+    response = requests.get(url).json()
+    if response['status'] == 'fail':
+        return "IP не найден!"
+    
+    result = f"""
+IP: {response['query']}
+Страна: {response['country']}
+Город: {response['city']}
+Провайдер: {response['isp']}
+Координаты: {response['lat']}, {response['lon']}
+"""
+    return result
+
+# Логируем команду /ip
+@bot.message_handler(commands=['ip'])
+def ip_command(message):
+    try:
+        ip = message.text.split()[1]
+        result = ip_lookup(ip)
+        logging.info(f'Пользователь {message.from_user.id} сделал IP lookup: {ip}')
+        bot.send_message(message.chat.id, result)
+    except:
+        bot.send_message(message.chat.id, "Использование: /ip [ip адрес]")
+
+# Логируем все остальные сообщения
+@bot.message_handler(func=lambda message: True)
+def log_all(message):
+    logging.info(f'Пользователь {message.from_user.id} написал сообщение: {message.text}')
+
+# Запуск бота
 bot.polling(none_stop=True, interval=0)
+
     
